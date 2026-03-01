71d44abb221edc34536dd1b9f71cd68fdbab34b9
Raju Pusti <rajupusti@yahoo.com>
2026-03-01 10:55:09 +0000

    improve success message

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.getElementById("cmd-send").addEventListener("click", function() {
-    let data = document.getElementById("cmd-input").value;
+    let inputElement = document.getElementById("cmd-input");
+    let data = inputElement.value;
     console.log(typeof(data));
     const url = "https://script.google.com/macros/s/AKfycbx7DcyopCwKC4TUiG0LqU4mDZpyNrEUdBdaJYHlOgxRlG2YMByoT6vcCDa1nJyuf702/exec"
 
@@ -13,8 +14,10 @@ document.getElementById("cmd-send").addEventListener("click", function() {
     })
    
     .then(result => {
-        console.log("Command sent successfully:", result.status);
+        console.log("Message sent successfully:", result.status);
         //result is a object and result.status is 200
+        inputElement.value = "";
+        
     })
     .catch(error => {
         console.error("Error sending command:", error);
