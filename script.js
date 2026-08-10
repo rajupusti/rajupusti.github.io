af9ab8f9372319e52b80c108022ea43533445ef2
Raju Pusti <rajupusti@yahoo.com>
2026-08-10 13:24:21 +0000

    ADD UPDATE

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.getElementById("send-btn").addEventListener("click", function() {
         headers: {
         "Content-Type": "application/x-www-form-urlencoded"
   },
-        body: JSON.stringify(data);
+        body: JSON.stringify(data),
         cors: "no-cors"
     })
    
